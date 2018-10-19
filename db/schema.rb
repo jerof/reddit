d327f1ec7e644d99031bfa62f1fa280c7f42b081
Nikhil <nikhilsamuel.89@gmail.com>
2018-10-19 11:38:35 -0400

    Add name field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_151228) do
+ActiveRecord::Schema.define(version: 2018_10_19_153154) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_151228) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
